ef0146f27a1036fe9b1f573141dfe625e5192fa8
victorsteven <chikodi543@gmail.com>
2020-07-29 23:56:13 +0100

    Next big number

---
+++ next_big_number/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+//Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+//12 ==> 21
+//513 ==> 531
+//2017 ==> 2071
+
+
+func main() {
+
+	//sepNumbers(409)
+	//fmt.Println(play(480))
+
+	//fmt.Println(intToSlice(34575))
+	//fmt.Println(sliceToInt([]int{1,2,3,4}))
+
+	fmt.Println(NextBigger(3452))
+
+}
+
+func intToSlice(n int) []int {
+	var numb []int
+	for n > 0 {
+		numb = append(numb, n % 10)
+		n /= 10
+	}
+	return numb
+}
+
+func sliceToInt(numb []int) int {
+	n := 0
+	for i, v := range numb {
+		//fmt.Println("the ints: ", int(math.Pow10(i))) //1, 10, 100, 1000
+		n += v * int(math.Pow10(i))
+	}
+	return n
+}
+
+func NextBigger(n int) int {
+	numb := intToSlice(n)
+	i := 0
+	for i := 0; i < len(numb) - 1; i++ {
+		if numb[i] > numb[i + 1] {
+			break
+		}
+	}
+	if i == len(numb)-1 {
+		return -1
+	}
+
+	mn := i
+
+	for j := 0; j < i; j++ {
+		if numb[j] > numb[i + 1] {
+			mn = j
+		}
+	}
+	numb[mn], numb[i+1] = numb[i + 1], numb[mn]
+	tmp := numb[:i + 1]
+	sort.Slice(tmp, func(i, j int) bool {
+		return tmp[i] > tmp[j]
+	})
+	return sliceToInt(numb)
+}
+
+
+
+func countDigit(i int) (count int) {
+	for i != 0 {
+		i = i /10
+		fmt.Println(i)
+		//i /= 10
+		count++
+	}
+	return count
+}
+
+func sepNumbers(n int) {
+
+	ans := strings.Split(string(n), "")
+	fmt.Println(ans)
+}
+
+func strDigits(n, digits int) (string, int) {
+	s := strconv.Itoa(n)
+	if s[0] == '-' {
+		s = s[1:]
+	}
+	if digits > len(s) {
+		digits = len(s)
+	}
+	if digits < 0 {
+		digits = 0
+	}
+	return s, digits
+}
+
+func play(n int) int {
+
+	nums := strconv.Itoa(n)
+
+	if len(nums) <= 0 {
+		return -1
+	}
+	if len(nums) == 1 {
+		return n
+	}
+
+	//register the first number, and assume it is the biggest:
+	strs := make([]string, 0)
+	strs = append(strs, nums[:1])
+
+	for i, v := range nums {
+		fmt.Println("the v: ", v)
+		fmt.Println("the str: ", strs[i])
+	}
+
+	return 999
+}
